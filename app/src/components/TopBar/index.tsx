22297233202e738932ed2d76c5ad020630731543
Renan Lopes <905renan@gmail.com>
2020-10-09 12:28:01 -0300

    WD | Topbar style

--- app/src/components/TopBar/index.tsx
+++ app/src/components/TopBar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Badge, Button } from '@material-ui/core';
-import { Settings, DirectionsBus, Update, GolfCourse } from '@material-ui/icons' ;
+import { Badge } from '@material-ui/core';
+import { DirectionsBus, Update, GolfCourse } from '@material-ui/icons' ;
 
 import { useSelector } from 'react-redux';
 import { StoreState } from '../../store/createStore';
@@ -15,16 +15,18 @@ const TopBar: React.FC = () => {
 	
     return(
 		<>
-			<Badge className='iconStyle' color="secondary" max={100000} aria-label="Número de Paradas no Mapa"  badgeContent={totalParadas}>
-				<GolfCourse color='secondary'/>
-			</Badge>
-			<Badge className='iconStyle' color="secondary"  aria-label="Hora da Ultima Atualização" badgeContent={updateTime}>
-				<Update color='secondary'/>
-			</Badge>
-			<Badge className='iconStyle' color="secondary"  max={100000} badgeContent={totalCars}>
-				<DirectionsBus aria-label="Número de Veiculos no Mapa" color='secondary'/>
-			</Badge>
-			<Button startIcon={<Settings />} disabled variant='outlined' size="small">Configurações</Button>
+			<div className='infoAreas'>
+				<Badge className='iconStyle' color="secondary" max={100000} aria-label="Número de Paradas no Mapa"  badgeContent={totalParadas}>
+					<GolfCourse color='secondary'/>
+				</Badge>
+				<Badge className='iconStyle' color="secondary"  max={100000} badgeContent={totalCars}>
+					<DirectionsBus aria-label="Número de Veiculos no Mapa" color='secondary'/>
+				</Badge>
+				<Badge className='iconStyle' color="secondary"  aria-label="Hora da Ultima Atualização" badgeContent={(updateTime === '') ? 0 : updateTime}>
+					<Update color='secondary'/>
+				</Badge>
+
+			</div>
 		</>
     );
 }
